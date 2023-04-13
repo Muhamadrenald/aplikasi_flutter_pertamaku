847e306275e044c6d19bc41793d8dc84f059e7ae
Muhamadrenald <avryso@gmail.com>
2023-04-13 20:02:13 +0700

    first commit

---
+++ lib/ui/poli_detail.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../model/poli.dart';
+
+class PoliDetail extends StatefulWidget {
+  const PoliDetail({super.key});
+
+  @override
+  State<PoliDetail> createState() => _PoliDetailState();
+}
+
+class _PoliDetailState extends State<PoliDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Detail Poli")),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            "Nama Poli : ${widget.poli.namaPoli}",
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  child: const Text("Ubah")),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const Text("Hapus")),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
